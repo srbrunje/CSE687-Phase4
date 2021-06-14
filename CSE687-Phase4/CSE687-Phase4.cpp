5f2a1a6cb65f049d6a1b938a8951391af9f97567
Leo Garza <leog4za@gmail.com>
2021-06-14 16:58:22 -0500

    Update CSE687-Phase4.cpp

--- CSE687-Phase4/CSE687-Phase4.cpp
+++ CSE687-Phase4/CSE687-Phase4.cpp
@@ -1,5 +1,4 @@
 // CSE687_Phase4.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 
 #include "Menu.h"
@@ -7,14 +6,10 @@
 
 int main() {
 
-
-
- 
     while (true) {
         RunMenu();
         system("cls");
     }
- 
 
     return 0;
 }
